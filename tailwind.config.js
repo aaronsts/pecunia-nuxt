d2f956e3739f81117b3394ffe34336d0d3ea68a3
Aaron <aaron.staes@gmail.com>
2024-04-04 15:35:21 -0500

    fix: remove unused packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const animate = require("tailwindcss-animate");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ["class"],
@@ -54,33 +52,6 @@ module.exports = {
 				DEFAULT: "hsl(40, 8%, 10%)",
 			},
 		},
-
-		extend: {
-			keyframes: {
-				"accordion-down": {
-					from: { height: 0 },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				"accordion-up": {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: 0 },
-				},
-				"collapsible-down": {
-					from: { height: 0 },
-					to: { height: "var(--radix-collapsible-content-height)" },
-				},
-				"collapsible-up": {
-					from: { height: "var(--radix-collapsible-content-height)" },
-					to: { height: 0 },
-				},
-			},
-			animation: {
-				"accordion-down": "accordion-down 0.2s ease-out",
-				"accordion-up": "accordion-up 0.2s ease-out",
-				"collapsible-down": "collapsible-down 0.2s ease-in-out",
-				"collapsible-up": "collapsible-up 0.2s ease-in-out",
-			},
-		},
 	},
-	plugins: [animate, require("@tailwindcss/forms")],
+	plugins: [require("@tailwindcss/forms")],
 };
