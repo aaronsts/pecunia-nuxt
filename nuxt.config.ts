33a0383f729311401889c5a42b3fed9d58289ea8
Aaron <aaron.staes@gmail.com>
2024-04-09 12:39:20 -0500

    feat: added shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+	modules: [
+		"@nuxtjs/supabase",
+		"@nuxtjs/tailwindcss",
+		"@pinia/nuxt",
+		"shadcn-nuxt",
+	],
 	devtools: { enabled: true },
 	components: [{ path: "~/components", pathPrefix: false }],
 	tailwindcss: {
 		viewer: false,
 	},
-	modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "@pinia/nuxt"],
+	shadcn: {
+		/**
+		 * Prefix for all the imported component
+		 */
+		prefix: "",
+		/**
+		 * Directory that the component lives in.
+		 * @default "./components/ui"
+		 */
+		componentDir: "./components/ui",
+	},
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,86 @@
+const animate = require("tailwindcss-animate")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	darkMode: ["class"],
-
-	theme: {
-		colors: {
-			primary: {
-				DEFAULT: "hsl(191, 88%, 24%)",
-				100: "hsl(188, 100%, 33%)",
-				200: "hsl(189, 97%, 26%)",
-				300: "hsl(191, 88%, 24%)",
-				400: "hsl(193, 100%, 16%)",
-			},
-			secondary: {
-				DEFAULT: "hsl(85, 40%, 55%)",
-				100: "hsl(84, 46%, 80%)",
-				200: "hsl(84, 44%, 67%)",
-				300: "hsl(85, 40%, 55%)",
-				400: "hsl(86, 42%, 44%)",
-			},
-
-			success: {
-				DEFAULT: "hsl(125, 50%, 55%)",
-			},
-			danger: {
-				DEFAULT: "hsl(0, 50%, 55%)",
-			},
-			info: {
-				DEFAULT: "hsl(185, 50%, 55%)",
-			},
-			warning: {
-				DEFAULT: "hsl(48, 60%, 55%)",
-			},
-
-			neutral: {
-				50: "hsl(60, 10%, 96%)",
-				100: "hsl(60, 10%, 92%)",
-				200: "hsl(60, 9%, 83%)",
-				300: "hsl(60, 9%, 72%)",
-				400: "hsl(54, 10%, 61%)",
-				500: "hsl(50, 10%, 53%)",
-				600: "hsl(49, 9%, 48%)",
-				700: "hsl(43, 9%, 40%)",
-				800: "hsl(43, 8%, 34%)",
-				900: "hsl(36, 7%, 28%)",
-				950: "hsl(40, 8%, 10%)",
-			},
-			white: {
-				DEFAULT: "hsl(60, 10%, 96%)",
-			},
-			black: {
-				DEFAULT: "hsl(40, 8%, 10%)",
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/forms")],
-};
+  darkMode: ["class"],
+  safelist: ["dark"],
+  prefix: "",
+  
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+      	xl: "calc(var(--radius) + 4px)",
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+        "collapsible-down": {
+          from: { height: 0 },
+          to: { height: 'var(--radix-collapsible-content-height)' },
+        },
+        "collapsible-up": {
+          from: { height: 'var(--radix-collapsible-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.2s ease-in-out",
+        "collapsible-up": "collapsible-up 0.2s ease-in-out",
+      },
+    },
+  },
+  plugins: [animate],
+}
