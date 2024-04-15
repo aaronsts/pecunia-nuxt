17f18f6c386883e654e6201d06820e0a0ce0cdc9
Aaron <aaron.staes@gmail.com>
2024-04-15 11:44:14 -0500

    feat: add header component

---
+++ components/dashboard/NewTransaction.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+<template>
+	<div class="p-6">
+		<h3>New Expense</h3>
+		<Button>Add</Button>
+	</div>
+</template>
---
+++ components/ui/Header.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup></script>
+<template>
+	<header class="text-gray-700 space-y-6 border-b border-gray-300 pb-8">
+		<h1>Dashboard</h1>
+		<div class="flex gap-16">
+			<div>
+				<p class="text-gray-500">Overview</p>
+				<h2>April</h2>
+			</div>
+			<div class="flex gap-12 text-center text-gray-500">
+				<div>
+					<p class="text-success text-xl">$3.000,00</p>
+					<p>Income</p>
+				</div>
+				<div>
+					<p class="text-danger text-xl">-$2.000,00</p>
+					<p>Expense</p>
+				</div>
+				<div>
+					<p class="text-gray-700 text-xl">$1.000,00</p>
+					<p>Total</p>
+				</div>
+			</div>
+		</div>
+	</header>
+</template>
